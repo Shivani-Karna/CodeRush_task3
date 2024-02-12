9a3490f06b920ab74c8ac5bcf171c6a0bc49130c
Shivani-Karna <cwanikarna@gmail.com>
2024-02-12 18:02:33 +0545

    Task-3 using Material_UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CountrySelect from "./MaterialUi/Autocomplete";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CountrySelect />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/MaterialUi/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <>index</>;
+};
+
+export default index;
