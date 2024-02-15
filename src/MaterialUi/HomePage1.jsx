faaeb9cf7f56896b18ada0c80dbb4ddd2c0ba285
Shivani-Karna <cwanikarna@gmail.com>
2024-02-15 16:26:25 +0545

    Description Added

--- src/MaterialUi/HomePage1.jsx
+++ src/MaterialUi/HomePage1.jsx
@@ -6,6 +6,10 @@ import {
   Grid,
   Container,
   Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from "@mui/material";
 import React from "react";
 const HomePage1 = () => {
@@ -24,20 +28,105 @@ const HomePage1 = () => {
               Posts
             </Typography>
             <Typography variant="h6" color="textSecondary" align="center">
-              Description Here
+              Code Rush works as a gateway to the convergence of talent and
+              opportunity. We create a seamless integration of talent into the
+              tech landscape through tailored curation and skill cultivation.
             </Typography>
             <Grid container justifyContent="center" spacing={2}>
               <Grid item>
                 <Button variant="contained">Primary Button</Button>
               </Grid>
-
               <Grid item>
                 <Button variant="contained">Secondary Button</Button>
               </Grid>
             </Grid>
           </Container>
+          <Grid mt={3}>
+            <Container>
+              <Grid container spacing={4}>
+                <Grid item>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image="https://source.unsplash.com/random"
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+                <Grid item>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image="https://source.unsplash.com/random"
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+                <Grid item>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image="https://source.unsplash.com/random"
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles,
+                        with over 6,000 species, ranging across all continents
+                        except Antarctica
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              </Grid>
+            </Container>
+          </Grid>
         </Grid>
       </main>
+      {/*Footer */}
+      <footer>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center">
+          Something here to give the footer a purpose!!
+        </Typography>
+      </footer>
     </React.Fragment>
   );
 };
